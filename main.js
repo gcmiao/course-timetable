6efa8241c03b9551bb5f943573cfb7aa1ce60e46
gcmiao <gc@roastew.com>
2018-09-03 22:33:49 +0200

    第二波，基本实现单元格合并

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 angular.module('index', []).controller('tableContent', function ($scope) {
     init($scope);
-    addCoursesToTable($scope.timeLineOfDays, $scope.selectedCourseIds, courses_data);
+    addCoursesToTable($scope, $scope.selectedCourseIds, courses_data);
     refreshTable($scope, courses_data);
 })
 
@@ -14,7 +14,8 @@ function init($scope) {
     $scope.todayIdxList = new Array();
     $scope.columnsOfTime = new Array();
     $scope.timeLineOfDays = CreatetimeLineOfDays();
-    $scope.selectedCourseIds = ["btcg", "rtg", "pba", "test1", "test2", "test3", "test4"];
+    $scope.selectedCourseIds = ["btcg", "rtg", "pba", "test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test10"];
+    //$scope.selectedCourseIds = ["btcg", "rtg", "pba", "test1", "test2", "test3", "test4"];
 
     var timePointList = new Array();
     var currentTime = START_TIME;
@@ -31,13 +32,57 @@ function init($scope) {
     }
 }
 
+function countValidCourseIdObj(courseIdObjs) {
+    var count = 0;
+    for (var idx in courseIdObjs) {
+        if (courseIdObjs[idx] != null) {
+            count++;
+        }
+    }
+    return count;
+}
+
 function refreshTable($scope, courses_data) {
     $scope.todayIdxList.length = 0;
     for (var i = 0; i < NUMBER_OF_DAY_PER_WEEK; i++) {
         var timeLineObjMap = $scope.timeLineOfDays[i];
         var headerSpan = 1;
+        var startObjList = new Array();
+        var followObjList = new Array();
         timeLineObjMap.forEach(function (timeLineObj) {
-            headerSpan = LCM(headerSpan, timeLineObj.courseIdObjs.length);
+            var overlapCount = countValidCourseIdObj(timeLineObj.courseIdObjs);
+            headerSpan = LCM(headerSpan, overlapCount);
+            startObjList.length = 0;
+            followObjList.length = 0;
+            var accumFollowColSpan = 0;
+            var avgFollowColSpan = headerSpan;
+            for (var idx in timeLineObj.courseIdObjs) {
+                var courseIdObj = timeLineObj.courseIdObjs[idx];
+                if (courseIdObj != null) {
+                    courseIdObj.startObj.overlapCount = Math.max(courseIdObj.startObj.overlapCount, overlapCount);
+                }
+                // if (courseIdObj.rowSpan > 0) {
+                //     startObjList.push(courseIdObj);
+                // }
+                // else
+                // {
+                //     followObjList.push(courseIdObj);
+                //     avgFollowColSpan = courseIdObj.startObj.colSpan;
+                //     accumFollowColSpan += avgFollowColSpan;
+                // }
+            }
+            // // has new courses start
+            // if (startObjList.length > 0)
+            // {
+            //     var avgFirstObjColSpan = (headerSpan - accumFollowColSpan) / startObjList.length;
+            //     if (avgFirstObjColSpan < avgFollowColSpan) {
+            //         s
+            //     }
+
+            // }
+            // else { // do not has new courses, evenly follow divide columns if has enough space
+
+            // }
         });
         $scope.dayHeaders[i].span = headerSpan;
     }
@@ -49,27 +94,56 @@ function refreshTable($scope, courses_data) {
     }
 }
 
-function getColumnsByTime ($scope, timeObj) {
+function getColumnsByTime($scope, timeObj) {
     var columns = new Array();
+    // travel days of the week
     for (var dayIdx in $scope.dayHeaders) {
         var header = $scope.dayHeaders[dayIdx];
         var timeLineObjMap = $scope.timeLineOfDays[dayIdx];
+        // has courses at current time point
         if (timeLineObjMap.has(timeObj.getHash())) {
             var timeLineObj = timeLineObjMap.get(timeObj.getHash());
-            var columnSpan = header.span / timeLineObj.courseIdObjs.length;
+            var preAccumColSpan = 0;
+            var lastAccumColSpan = 0;
+            // travel all overlap courses
             for (var courseIdx in timeLineObj.courseIdObjs) {
                 var courseIdObj = timeLineObj.courseIdObjs[courseIdx];
-                if (courseIdObj.rowSpan != 0) {
-                    columns.push({colSpan:columnSpan,            
-                                            rowSpan:courseIdObj.rowSpan,  
-                                            courseId:courseIdObj.id,
-                                            time:timeObj,
-                                            day:dayIdx});
+                if (courseIdObj == null) {
+                    continue;
                 }
+                var columnSpan = header.span / courseIdObj.startObj.overlapCount;
+                if (courseIdObj.overlapIdx >= 0) {
+                    courseIdObj.columnPos = preAccumColSpan;
+                    columns.push({
+                        colSpan: columnSpan, rowSpan: courseIdObj.rowSpan,
+                        courseId: courseIdObj.courseId, time: timeObj, day: dayIdx
+                    });
+                }
+                else
+                {
+                    var postAccumColSpan = courseIdObj.startObj.columnPos - preAccumColSpan;
+                    if (postAccumColSpan > 0) {
+                        columns.push({
+                            colSpan: postAccumColSpan, rowSpan: 1,
+                            courseId: "", time: timeObj, day: dayIdx
+                        });
+                        preAccumColSpan += postAccumColSpan;
+                    }
+                }
+                preAccumColSpan += columnSpan;
+            }
+            if (preAccumColSpan < header.span) {
+                columns.push({
+                    colSpan: header.span - preAccumColSpan, rowSpan: 1,
+                    courseId: "", time: timeObj, day: dayIdx
+                });
             }
         }
         else {
-            columns.push({colSpan:header.span, rowSpan:1, courseId:"", time:timeObj, day:dayIdx});
+            columns.push({
+                colSpan: header.span, rowSpan: 1,
+                courseId: "", time: timeObj, day: dayIdx
+            });
         }
     }
     return columns;
@@ -117,37 +191,115 @@ function pickTimeObj(timeObjMap, timeObj) {
     return obj;
 }
 
+function findMinAvailableOverlapIdx(courseIdObjs) {
+    for (var idx in courseIdObjs) {
+        if (courseIdObjs[idx] == null) {
+            return idx;
+        }
+    }
+    return courseIdObjs.length;
+}
+
+function expandArray(array, newSize, defaultValue) {
+    var offset = newSize - array.length;
+    while(offset > 0) {
+        array.push(defaultValue);
+        offset--;
+    }
+}
+
+function insertToCourseIdObjArray(objArray, pos, courseIdObj) {
+    expandArray(objArray, pos, null);
+    objArray.push(null);
+    for (var i = objArray.length - 1; i > pos; i--) {
+        objArray[i] = objArray[i - 1];
+    }
+    objArray[pos] = courseIdObj;
+}
+
 function addCourseToTimeObjs(timeLineObjMap, courseId, startTime, endTime) {
     var currentTime = startTime;
     var rowSpan = (endTime.getHash() - startTime.getHash()) / MIN_TIME_INTERVAL;
-    var firstObj = null;
+    var startObj = null;
     while (isEarlierThan(currentTime, endTime)) {
         var timeObj = pickTimeObj(timeLineObjMap, currentTime);
         var courseIdObj = CreateCourseIdObj(courseId, rowSpan);
-        if (firstObj == null) {
-            firstObj = courseIdObj;
+        var overlapIdx = 0;
+        if (startObj == null) {
+            startObj = courseIdObj;
+            overlapIdx = findMinAvailableOverlapIdx(timeObj.courseIdObjs);
         }
-        courseIdObj.firstObj = firstObj;
-        timeObj.courseIdObjs.push(courseIdObj);
+        else
+        {
+            overlapIdx = startObj.overlapIdx;
+        }
+        courseIdObj.startObj = startObj;
+        insertToCourseIdObjArray(timeObj.courseIdObjs, overlapIdx, courseIdObj);
         currentTime = tickToNextTime(currentTime);
         rowSpan = 0;
     }
 }
 
-function addCourseToTable(timeLineOfDays, courseId, courses_data) {
+function expandCourseFromStartToEnd(timeLineObjMap, startObj, startTime, endTime) {
+    var currentTime = tickToNextTime(startTime);
+    while(isEarlierThan(currentTime, endTime)) {
+        var timeObj = pickTimeObj(timeLineObjMap, currentTime);
+        var courseIdObj = CreateCourseIdObj(startObj.courseId, startObj.dateIdx);
+        courseIdObj.startObj = startObj;
+        insertToCourseIdObjArray(timeObj.courseIdObjs, startObj.overlapIdx, courseIdObj);
+        currentTime = tickToNextTime(currentTime);
+    }
+}
+
+// expand course from start time to end time
+function expandCoursesOnTable($scope, courses_data) {
+    // travel days of week
+    for (var dayIdx in $scope.dayHeaders) {
+        var header = $scope.dayHeaders[dayIdx];
+        var timeLineObjMap = $scope.timeLineOfDays[dayIdx];
+        // travel all time point
+        for (var timeIdx in $scope.timePointList) {
+            var timeObj = $scope.timePointList[timeIdx];
+            // has courses start at current time point
+            if (timeLineObjMap.has(timeObj.getHash())) {
+                var timeLineObj = timeLineObjMap.get(timeObj.getHash());
+                for (var courseIdx in timeLineObj.courseIdObjs) {
+                    var courseIdObj = timeLineObj.courseIdObjs[courseIdx];
+                    if (courseIdObj == null) {
+                        continue;
+                    }
+                    var dateInfo = courses_data[courseIdObj.courseId].dates[courseIdObj.dateIdx];
+                    var startTime = CreateTimeObjByString(dateInfo.startTime);
+                    // if start at this time point, then expand it to the end time point
+                    if (timeObj.getHash() == startTime.getHash()) {
+                        var endTime = CreateTimeObjByString(dateInfo.endTime);
+                        courseIdObj.rowSpan =  (endTime.getHash() - startTime.getHash()) / MIN_TIME_INTERVAL;
+                        courseIdObj.overlapIdx = courseIdx;
+                        expandCourseFromStartToEnd(timeLineObjMap, courseIdObj, startTime, endTime);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function addCourseStartToTable(timeLineOfDays, courseId, courses_data) {
     var courseData = courses_data[courseId];
     for (i in courseData.dates) {
         var dateInfo = courseData.dates[i];
         var startTime = CreateTimeObjByString(dateInfo.startTime);
-        var endTime = CreateTimeObjByString(dateInfo.endTime);
-        addCourseToTimeObjs(timeLineOfDays[dateInfo.day - 1], courseId, startTime, endTime);
+        var timeObj = pickTimeObj(timeLineOfDays[dateInfo.day - 1], startTime);
+        var courseIdObj = CreateCourseIdObj(courseId, i);
+        courseIdObj.startObj = courseIdObj;
+        timeObj.courseIdObjs.push(courseIdObj);
     }
 }
 
-function addCoursesToTable(timeLineOfDays, courseIds, courses_data) {
+function addCoursesToTable($scope, courseIds, courses_data) {
     for (i in courseIds) {
-        addCourseToTable(timeLineOfDays, courseIds[i], courses_data);
+        addCourseStartToTable($scope.timeLineOfDays, courseIds[i], courses_data);
     }
+    expandCoursesOnTable($scope, courses_data)
 }
 
 function TimeObj(hh, mm) {
@@ -176,13 +328,19 @@ function TimeLineObj() {
     this.courseIdObjs = new Array();
 }
 
-function CourseIdObj(id, rowSpan) {
-    this.id = id;
-    this.rowSpan = rowSpan;
+function CourseIdObj(id, dateIdx) {
+    this.courseId = id;
+    this.dateIdx = dateIdx;
+    this.rowSpan = 1;
+    this.colSpan = 1;
+    this.columnPos = 0;
+    this.startObj = null;
+    this.overlapCount = 1;
+    this.overlapIdx = -1;
 }
 
-function CreateCourseIdObj(id, rowSpan) {
-    return new CourseIdObj(id, rowSpan);
+function CreateCourseIdObj(id, dateIdx) {
+    return new CourseIdObj(id, dateIdx);
 }
 
 function GCD(a, b) {
